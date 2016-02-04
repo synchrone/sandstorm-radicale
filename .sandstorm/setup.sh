92d73b03259d7e63c05d97060a2d1c6efc7594a0
Aleksandr Bogdanov <syn@syn.im>
2016-02-04 13:22:59 +0100

    Using absolute path to patch int-it's HTML, and a version variable

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+INF_IT_VER=0.13.1
 set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
@@ -14,16 +15,16 @@ mkdir -p /var/log/radicale
 ln -sf /opt/app/radicale.config /etc/radicale/config
 ln -sf /opt/app/radicale.rights /etc/radicale/rights
 
-#download and configure CardDavMATE
-curl -s https://www.inf-it.com/CardDavMATE_0.13.1.zip > /opt/app/carddavmate.zip
+echo "Downloading and configuring CardDavMATE"
+curl -s https://www.inf-it.com/CardDavMATE_$INF_IT_VER.zip > /opt/app/carddavmate.zip
 unzip -q -o /opt/app/carddavmate.zip -d /opt/app
 ln -sf /opt/app/carddav.config.js /opt/app/carddavmate/config.js
 sed -i -e 's/block/none/' /opt/app/carddavmate/cache_handler.js
-sed -i -e ':a;N;$!ba;s/\/css" \/>\n\t<script/\/css" \/>\n\t<link rel="stylesheet" href="css\/hideresources.css" type="text\/css" \/>\n<script/' carddavmate/index.html
+sed -i -e ':a;N;$!ba;s/\/css" \/>\n\t<script/\/css" \/>\n\t<link rel="stylesheet" href="css\/hideresources.css" type="text\/css" \/>\n<script/' /opt/app/carddavmate/index.html
 
-#download and configure CalDavZAP
-curl -s https://www.inf-it.com/CalDavZAP_0.13.1.zip > /opt/app/caldavzap.zip
+echo "Downloading and configuring CalDavZAP..."
+curl -s https://www.inf-it.com/CalDavZAP_$INF_IT_VER.zip > /opt/app/caldavzap.zip
 unzip -q -o /opt/app/caldavzap.zip -d /opt/app
 ln -sf /opt/app/caldav.config.js /opt/app/caldavzap/config.js
 sed -i -e 's/block/none/' /opt/app/caldavzap/cache_handler.js
-sed -i -e ':a;N;$!ba;s/\/css" \/>\n\t<script/\/css" \/>\n\t<link rel="stylesheet" href="css\/hideresources.css" type="text\/css" \/>\n<script/' caldavzap/index.html
+sed -i -e ':a;N;$!ba;s/\/css" \/>\n\t<script/\/css" \/>\n\t<link rel="stylesheet" href="css\/hideresources.css" type="text\/css" \/>\n<script/' /opt/app/caldavzap/index.html
