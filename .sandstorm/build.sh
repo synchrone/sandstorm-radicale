9551260e67b581df286bc7c4b7cb10538fa01dad
Aleksandr Bogdanov <syn@syn.im>
2016-07-31 13:27:27 +0200

    Fixing issue #21

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -11,6 +11,9 @@ if [ -f /opt/app/requirements.txt ] ; then
     $VENV/bin/pip install -r /opt/app/requirements.txt
 fi
 
+# See: https://github.com/synchrone/sandstorm-radicale/issues/21
+bash -c "patch -p1 -i /opt/app/issue-21.diff --forward -d $VENV/lib/python3.4/site-packages/; exit 0"
+
 echo "Generating NGINX configs..."
 #rebuilding nginx config for dev
 sed --expression 's/_subapp_/caldavzap/' /opt/app/nginx.conf.tmpl > /opt/app/caldav.nginx.conf;
